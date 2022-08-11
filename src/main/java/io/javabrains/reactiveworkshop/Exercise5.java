907450ac3eb360875d543f8f109b193dee293410
Aidor777 <guillaume.garin@live.com>
2022-08-11 16:29:06 +0200

    Exercise 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,21 +1,45 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                number -> System.out.println("Number " + number),
+                error -> System.out.println(error.getMessage()),
+                () -> System.out.println("Complete!")
+        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+    // When extending BaseSubscriber, we need to tell the source that we are ready to receive n items with request(n).
+    // As the next items do not arrive "automatically", this is a good way to handle back-pressure.
+    private static class MySubscriber<T> extends BaseSubscriber<T> {
+
+        @Override
+        protected void hookOnSubscribe(Subscription subscription) {
+            System.out.println("Subscribed!");
+            request(1);
+        }
+
+        @Override
+        protected void hookOnNext(T value) {
+            System.out.println("Received: " + value);
+            request(1);
+        }
     }
 
 }
